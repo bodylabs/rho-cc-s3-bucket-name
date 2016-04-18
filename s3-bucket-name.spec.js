ff818e941b95390781ae10c3d2355d61e0297527
Paul Melnikow <paul.melnikow@bodylabs.com>
2016-04-18 18:24:51 -0400

    Remove my inferior tests

--- s3-bucket-name.spec.js
+++ s3-bucket-name.spec.js
@@ -1,25 +1,8 @@
-var c = require('rho-contracts'),
-    s3BucketName = require('./s3-bucket-name');
+var s3BucketName = require('./s3-bucket-name');
 
 var should = require('should');
 
-// For convenience and conciseness.
-var good = should.doesNotThrow;
-var bad = function (block) { should.throws(block, c.ContractError); };
-
 describe('s3BucketName', function () {
-
-    context('invoked with a good bucket name', function () {
-        it('does not raise a contract error', function () {
-            good(function () { s3BucketName.check('frodo'); });
-        });
-    });
-
-    context('invoked with a bad bucket name', function () {
-        it('raises a contract error', function () {
-            bad(function () { s3BucketName.check('_nope'); });
-        });
-    });
 
     it('accepts a single valid label', function () {
         s3BucketName.check('first-label01');
