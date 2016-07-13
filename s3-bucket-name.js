cb1627badedb5a46073868a08fe43c64fe05976c
Paul Melnikow <github@paulmelnikow.com>
2016-07-13 10:23:44 -0400

    Fix lingering reference to rho-contracts-fork (#5)

--- s3-bucket-name.js
+++ s3-bucket-name.js
@@ -1,4 +1,4 @@
-var c = require('rho-contracts');
+var c = require('rho-contracts-fork');
 
 // http://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
 var s3BucketName = c.matches(/^([a-z0-9][-a-z0-9]+)(\.[a-z0-9][-a-z0-9]+)*$/)
